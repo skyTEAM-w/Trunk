3044ae12ebbdc00584554185b55f0bcde19fbb2a
WuChengpei_sky <w2563070231@outlook.com>
2023-11-25 13:27:35 +0800

    README.md

--- src/test/java/com/test/mysql/jdbcTest.java
+++ src/test/java/com/test/mysql/jdbcTest.java
@@ -9,13 +9,13 @@ public class jdbcTest {
     public static void main(String[] args) throws SQLException {
         // 1. 加载驱动
         try {
-            Class.forName("com.mysql.cj.jdbc.Dr`iver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users?serverTimezone=UTC&useSSL=false", "root", "040331");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/users?serverTimezone=UTC& useSSL=false", "root", "040331");
         System.out.println(connection);
-        String sql = "insert into users(id, password) value(123, 123456)";
+        String sql = "insert into users(id, password) value(555, 123456)";
         PreparedStatement statement = connection.prepareStatement(sql);
         statement.executeUpdate();
         connection.close();
