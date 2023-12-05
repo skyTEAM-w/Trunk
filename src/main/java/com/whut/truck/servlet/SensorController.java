348cf237f57e6d7d1d35ad6ccd15ea61b4597a40
yihaiqianxun <xsal98427348@163.com>
2023-12-05 20:33:48 +0800

    查询功能

--- src/main/java/com/whut/truck/servlet/SensorController.java
+++ src/main/java/com/whut/truck/servlet/SensorController.java
@@ -14,26 +14,7 @@ import java.util.Scanner;
 public class SensorController {
     private SensorService sensorService = new SensorServiceImpl();
     protected void csv_save() throws IOException {                //添加csv
-
-        String csvFile = "D:/000001/1/train_FD001.csv";
-        String line;
-        String csvSplitBy = ",";
-        String[] data = new String[0];
-
-        int counter = 0; // 计数器，用于限制读取的行数
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            while ((line = br.readLine()) != null && counter < 31) {
-                data = line.split(csvSplitBy);
-                if(counter!=0) {
-                    this.sensorService.csv_save(new Sensor(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16], data[17], data[18], data[19], data[20], data[21], data[22], data[23], data[24], data[25], data[26]));
-                }
-                counter++;
-                data = new String[0];
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(Arrays.toString(data));
+        this.sensorService.csv_save(null);
         System.out.println("插入成功");
     }
 
