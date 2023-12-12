d8ff8b69a6f512565e48bbe07f27ccb4d208f7c9
Laity9 <329340@whut.edu.cn>
2023-12-12 21:52:19 +0800

    新增页面传感器数据

--- src/main/java/com/whut/truck/servlet/SensorController.java
+++ src/main/java/com/whut/truck/servlet/SensorController.java
@@ -1,19 +1,94 @@
 package com.whut.truck.servlet;
 
+import com.whut.truck.Dao.VehicleStatusDao;
+import com.whut.truck.Dao.impl.VehicleStatusDaoImpl;
 import com.whut.truck.Dto.SensorDto;
+import com.whut.truck.Dto.VehicleStatusDto;
 import com.whut.truck.Service.impl.SensorServiceImpl;
 import com.whut.truck.entity.Sensor;
 import com.whut.truck.Service.SensorService;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.crypto.Data;
 import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
-
-public class SensorController {
+@WebServlet(name = "SensorController", value = "/SensorController")
+public class SensorController extends HttpServlet {
     private SensorService sensorService = new SensorServiceImpl();
-    public void csv_save(InputStream inputStream) throws IOException {                //添加csv
-        this.sensorService.csv_save(inputStream);
-        System.out.println("插入成功");
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 从请求参数中获取车辆编号
+        request.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("utf-8");
+        response.setContentType("text/html;charset=utf-8");
+
+        String VehicleID = request.getParameter("VehicleName");
+        String cycle = request.getParameter("cycle");
+        String[] data = new String[27];
+        System.arraycopy(output_one_lineBY_id(VehicleID, cycle),0,data,0,27);
+
+        String result = data[0] + "\n";
+        request.setAttribute("Data0", result);
+        result = data[1] + "\n";
+        request.setAttribute("Data1", result);
+        result = data[2] + "\n";
+        request.setAttribute("Data2", result);
+        result = data[3] + "\n";
+        request.setAttribute("Data3", result);
+        result = data[4] + "\n";
+        request.setAttribute("Data4", result);
+        result = data[5] + "\n";
+        request.setAttribute("Data5", result);
+        result = data[6] + "\n";
+        request.setAttribute("Data6", result);
+        result = data[7] + "\n";
+        request.setAttribute("Data7", result);
+        result = data[8] + "\n";
+        request.setAttribute("Data8", result);
+        result = data[9] + "\n";
+        request.setAttribute("Data9", result);
+        result = data[10] + "\n";
+        request.setAttribute("Data10", result);
+        result = data[11] + "\n";
+        request.setAttribute("Data11", result);
+        result = data[12] + "\n";
+        request.setAttribute("Data12", result);
+        result = data[13] + "\n";
+        request.setAttribute("Data13", result);
+        result = data[14] + "\n";
+        request.setAttribute("Data14", result);
+        result = data[15] + "\n";
+        request.setAttribute("Data15", result);
+        result = data[16] + "\n";
+        request.setAttribute("Data16", result);
+        result = data[17] + "\n";
+        request.setAttribute("Data17", result);
+        result = data[18] + "\n";
+        request.setAttribute("Data18", result);
+        result = data[19] + "\n";
+        request.setAttribute("Data19", result);
+        result = data[20] + "\n";
+        request.setAttribute("Data20", result);
+        result = data[21] + "\n";
+        request.setAttribute("Data21", result);
+        result = data[22] + "\n";
+        request.setAttribute("Data22", result);
+        result = data[23] + "\n";
+        request.setAttribute("Data23", result);
+        result = data[24] + "\n";
+        request.setAttribute("Data24", result);
+        result = data[25] + "\n";
+        request.setAttribute("Data25", result);
+        result = data[26] + "\n";
+        request.setAttribute("Data26", result);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/VehicleSensor.jsp");
+        dispatcher.forward(request, response);
     }
 
     /**
@@ -27,7 +102,6 @@ public class SensorController {
         Scanner scanner_quantity = new Scanner(System.in);
         String input_id = scanner_quantity.nextLine();*/
         SensorDto sensorDto = this.sensorService.csv_find(id);
-
         return sensorDto.getSensorStream();
     }
 
